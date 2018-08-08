4cdde121aea1f723adcd07659b4e315baeb4bf9f
Quan Zhuo <zhuo.quan@outlook.com>
2018-08-09 07:51:38 +0800

    change

---
+++ app/src/main/java/com/github/quanzhuo/pkgviewer/SearchResultActivity.java
@@ -0,0 +1,13 @@
+package com.github.quanzhuo.pkgviewer;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SearchResultActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_result);
+    }
+}
---
+++ app/src/main/java/com/github/quanzhuo/pkgviewer/SearchResultFragment.java
@@ -0,0 +1,7 @@
+package com.github.quanzhuo.pkgviewer;
+
+import android.support.v4.app.Fragment;
+
+public class SearchResultFragment extends Fragment {
+
+}
